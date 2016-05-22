f44039a8da997b59d0aa06e4c08814f8b3ff9dc3
Yuki Numakura <numakura.yuki@tayutau.co.jp>
2016-05-22 10:35:21 +0900

    破壊エフェクト追加

--- Assets/Scripts/TargetEnemy.cs
+++ Assets/Scripts/TargetEnemy.cs
@@ -1,1 +1,16 @@
-﻿using UnityEngine;using System.Collections;public class TargetEnemy : MonoBehaviour {	void Start(){		TargetEnemies.insntace.Add(gameObject);	}}
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetEnemy : MonoBehaviour {
+	[SerializeField]
+	GameObject deadEffectPrefab;
+	void Start(){
+		TargetEnemies.insntace.Add(gameObject);
+	}
+	void OnDestroy(){
+		if(deadEffectPrefab){
+			var eff = Instantiate(deadEffectPrefab);
+			eff.transform.position = this.transform.position;
+		}
+	}
+}
