0bbd7b697887b65b171ffc155d1036b009e0755d
be-ta <dcpsdcpc@gmail.com>
2016-05-22 12:22:19 +0900

    Modigy Fighter Operate

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -1,13 +1,35 @@
-﻿using UnityEngine;using System.Collections;using Leap;public class Fighter : MonoBehaviour {    Controller controller = new Controller();   public int FingerCount;  //public GameObject[] FingerObjects;  public Vector handAngle;   void Start()  {  }   void Update()  {    Frame frame = controller.Frame();    Hand hand = frame.Hands.Rightmost;
+﻿using UnityEngine;using System.Collections;using Leap;public class Fighter : MonoBehaviour {
 
-    //右手がLeap上に存在する
-    if (hand.IsValid && hand.IsRight)    {
-        handAngle = hand.Direction;
-        //transform.LookAt(ToVector3(handAngle));
-        //transform.position += transform.forward * -0.01f;
-        transform.forward = ToVector3(handAngle).normalized;
+    public Controller controller = new Controller();
+    public GameObject FighterObject;
+    public Quaternion basisRotation;
+
+    void Start()
+    {
+        basisRotation = FighterObject.transform.rotation;
     }
 
-    transform.position += transform.forward * -1.0f;
+    void Update()    {
+        Frame frame = controller.Frame();
+        HandList hands = frame.Hands;
+        Hand hand = hands.Rightmost;
+        InteractionBox interactionBox = frame.InteractionBox;
 
-}   void SetVisible( GameObject obj, bool visible )  {    foreach( Renderer component in obj.GetComponents<Renderer>() ) {      component.enabled = visible;    }  }   Vector3 ToVector3( Vector v )  {    return new UnityEngine.Vector3( v.x, -v.y, -v.z );  }}
+        //右手がLeap上に存在する
+        if (hand.IsValid && hand.IsRight)        {
+            Vector3 cross1 = Vector3.Cross( transform.up, ToVector3(hand.PalmNormal).normalized * -1.0f);
+            float f = Vector3.Angle( transform.up, ToVector3(hand.PalmNormal).normalized * -1.0f );
+            transform.Rotate( cross1, f );
+       
+            Vector3 cross2 = Vector3.Cross( transform.forward, ToVector3(hand.Direction).normalized);
+            float f2 = Vector3.Angle( transform.forward, ToVector3(hand.Direction).normalized);
+            transform.Rotate(cross2, f2);
+
+            Vector3 cross3 = Vector3.Cross( ToVector3(hand.Direction).normalized, ToVector3(hand.PalmNormal).normalized );
+
+            Vector3 cross4 = Vector3.Cross( transform.right, cross3);
+            float f3 = Vector3.Angle( transform.right, cross3 );
+            transform.Rotate(cross4, f3);
+        }
+
+        transform.position += transform.forward * -0.2f;    }   void SetVisible( GameObject obj, bool visible )  {    foreach( Renderer component in obj.GetComponents<Renderer>() ) {      component.enabled = visible;    }  }   Vector3 ToVector3( Vector v )  {    return new UnityEngine.Vector3(-1.0f * v.x, v.y, v.z );  }}
